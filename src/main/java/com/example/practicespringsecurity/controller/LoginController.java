6858ad1560d1eee20be94f81fb88ea3d098aceac
geniusBrainLsm <geniusbrainlsm@gmail.com>
2023-11-07 22:37:18 +0900

    study AuthenticationProvider

--- src/main/java/com/example/practicespringsecurity/controller/LoginController.java
+++ src/main/java/com/example/practicespringsecurity/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.example.practicespringsecurity.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
     private final CustomerRepository customerRepository;
-
+    private final PasswordEncoder passwordEncoder;
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
